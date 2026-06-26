5bb0fc1d7edaeff752d6822d86084027b4a4099c
Allen Porter <allen.porter@gmail.com>
2026-06-26 08:22:51 -0700

    fix(docs): list public submodules explicitly in __all__ to restore pdoc visibility

--- google_health_api/__init__.py
+++ google_health_api/__init__.py
@@ -2,6 +2,15 @@
 .. include:: ../README.md
 """
 
+from . import api, auth, client, const, exceptions, model
 from .api import GoogleHealthApi
 
-__all__ = ["GoogleHealthApi"]
+__all__ = [
+    "GoogleHealthApi",
+    "api",
+    "auth",
+    "client",
+    "const",
+    "exceptions",
+    "model",
+]
