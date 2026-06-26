61e7d0ccdd3a0801de6546840690ee28bdfd3c70
Allen Porter <allen.porter@gmail.com>
2026-06-26 09:08:40 -0700

    docs: export GoogleHealthApi first in __all__ for pydoc ordering

--- google_health_api/__init__.py
+++ google_health_api/__init__.py
@@ -2,9 +2,11 @@
 .. include:: ../README.md
 """
 
+from .api import GoogleHealthApi
 from . import api, auth, client, const, exceptions, model
 
 __all__ = [
+    "GoogleHealthApi",
     "api",
     "auth",
     "client",
