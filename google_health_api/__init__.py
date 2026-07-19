ea7c0d9c3bb15d7b836067bf2048c84e3d41006b
Allen Porter <allen.porter@gmail.com>
2026-07-19 15:44:33 -0700

    refactor: Remove WebhookVerifier from top-level namespace

--- google_health_api/__init__.py
+++ google_health_api/__init__.py
@@ -3,12 +3,10 @@
 """
 
 from .api import GoogleHealthApi
-from .webhook import WebhookVerifier
 from . import api, auth, client, const, exceptions, model, webhook
 
 __all__ = [
     "GoogleHealthApi",
-    "WebhookVerifier",
     "api",
     "auth",
     "client",
